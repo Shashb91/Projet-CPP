056d134db68a0b5a132590b31a955876debd70e5
ShashankanBala <shashankan.bala@icloud.com>
2025-12-24 13:06:52 +0100

    Créations des fichiers

---
+++ Adherent.h
@@ -0,0 +1,13 @@
+//
+// Created by shash on 24/12/2025.
+//
+
+#ifndef PROJET_CPP_ADHERENT_H
+#define PROJET_CPP_ADHERENT_H
+
+
+class Adherent {
+};
+
+
+#endif //PROJET_CPP_ADHERENT_H
---
+++ Bibliotheque.h
@@ -0,0 +1,18 @@
+//
+// Created by shash on 24/12/2025.
+//
+
+#ifndef PROJET_CPP_BIBLIOTHEQUE_H
+#define PROJET_CPP_BIBLIOTHEQUE_H
+
+
+class Bibliotheque {
+private:
+    String : nom;
+    String : adresse;
+    static int : code;
+    Livre* : liste;
+};
+
+
+#endif //PROJET_CPP_BIBLIOTHEQUE_H
