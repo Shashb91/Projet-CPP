de61002a5631cbd4922834f55c045edb69e61204
shash <shashankan.bala@icloud.com>
2025-12-26 15:37:49 +0100

    Début du code d'Adhérent

--- Adherent.cpp
+++ Adherent.cpp
@@ -1,5 +1,33 @@
-//
-// Created by shash on 24/12/2025.
-//
+// Code de la classe Adhérent
 
 #include "Adherent.h"
+#include <string>
+using std::string;
+#include <iostream>
+using namespace std;
+#include "Bibliotheque.h"
+#include "Livre.h"
+
+
+Adherent::Adherent(){numAdherent++; nom = NULL; prenom = NULL; adresse = NULL; bib = NULL; liste = NULL; maxEmprunt = 0; listeEmprunt = 0;}
+Adherent::Adherent(string nom_, string prenom_, string adresse_, Bibliotheque bib_, int maxEmprunt_){
+    numAdherent++;
+    nom = nom_;
+    prenom = prenom_;
+    adresse = adresse_;
+    bib = bib_;
+    maxEmprunt = maxEmprunt_;
+    liste = new Livre[maxEmprunt];
+    listeEmprunt = 0;
+}
+
+void Adherent::empruntLivre(string ISBN){
+    if ((listeEmprunt < maxEmprunt){
+        for (int i = 0; i < bib.get_; i++){}
+    }
+
+}
+
+void Adherent::retourLivre(string ISBN)
+{
+}
--- Adherent.h
+++ Adherent.h
@@ -1,12 +1,30 @@
-//
-// Created by shash on 24/12/2025.
-//
+// Header de la classe Adhérent
 
 #ifndef PROJET_CPP_ADHERENT_H
 #define PROJET_CPP_ADHERENT_H
+#include <string>
+using std::string;
+#include <iostream>
+using namespace std;
+#include "Bibliotheque.h"
+#include "Livre.h"
 
 
 class Adherent {
+private:
+    string nom;
+    string prenom;
+    string adresse;
+    static int numAdherent;
+    Bibliotheque bib;
+    Livre* liste;
+    int maxEmprunt;
+    int listeEmprunt;
+
+public:
+    Adherent(); Adherent(string nom_, string prenom_, string adresse_, Bibliotheque bib_, int maxEmprunt_);
+
+    void empruntLivre(string ISBN); void retourLivre(string ISBN);
 };
 
 
